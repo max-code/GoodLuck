4c4415e48e9ee2fcc9b22600e60c873c45077ef9
Max Jordan <max.jordan1306@gmail.com>
2023-12-10 16:56:27 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
 
 @app.route('/')
 def index():
-    return render_template('index.html', message="Hello from Flask!")
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if 'password' in session:
+        return redirect(url_for('game'))
+
+
+    if request.method == 'POST':
+        if request.form['password'] == 'p':  # Set your password
+            session['password'] = request.form['password']
+            return redirect(url_for('game'))
+        else:
+            return 'Invalid Password'
+    return render_template('login.html')  # Your login HTML page
+
+@app.route('/game')
+def game():
+    if 'password' in session:
+        return render_template('game.html')  # Your game HTML page
+    return redirect(url_for('login'))
+
+@app.route('/api/letter')
+def letter():
+    # Logic to select a letter
+    return jsonify({'letter': 'A'})  # Example letter
 
 if __name__ == '__main__':
     app.run(debug=True)
